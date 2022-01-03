bc5137a819e0406772b07af2d281da3601b35329
neda <nedaakbari555@gmail.com>
2022-01-04 00:37:02 +0330

    add Offer class and add this property to Orders class

---
+++ src/main/java/ir/maktab/homeServiceProvider/model/entity/Offer.java
@@ -0,0 +1,34 @@
+package ir.maktab.homeServiceProvider.model.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Entity
+@Data
+public class Offer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long proposedPriceOffer;
+    private String description;
+    private int duringTime;
+
+    private LocalTime startWorkTime;
+
+    @CreationTimestamp
+    private Date submissionDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date offerDate;
+
+    @ManyToOne
+    private Expert expert;
+
+    @ManyToOne
+    private Orders orders;
+}
