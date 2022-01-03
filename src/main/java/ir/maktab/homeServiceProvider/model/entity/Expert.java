234081b02d370ee6f7ddddc7fe19719ca2b23f15
neda <nedaakbari555@gmail.com>
2022-01-04 00:28:45 +0330

    change variable of Expert class and add ImageWrapper class for upload picture

--- src/main/java/ir/maktab/homeServiceProvider/model/entity/Expert.java
+++ src/main/java/ir/maktab/homeServiceProvider/model/entity/Expert.java
@@ -1,8 +1,10 @@
 package ir.maktab.homeServiceProvider.model.entity;
 
+import ir.maktab.homeServiceProvider.model.entity.service.MainService;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,14 +12,55 @@ import java.util.List;
 @Entity
 @Data
 public class Expert extends User{
-   // private Blob picture;
+    //private Blob picture;
+    private byte[] image;
+
     private int Score;
-    @ManyToMany(mappedBy = "experts")
-    private List<SubService> services = new ArrayList<>();
+
+   /* @ElementCollection
+    @CollectionTable
+    @MapKeyColumn(name = "order_id")//  اسم ستون متعلق به کی
+    @Column(name = "score_value")//اسم متعلق به ولیو
+    private Map<Orders, Integer> score = new HashMap<>();*/
+
+    @ManyToMany(mappedBy = "experts", fetch = FetchType.EAGER)
+    private List<MainService> mainServices = new ArrayList<>();
+
+    /*public Expert() {
+        saveImage();
+    }*/
 
     @Override
     public String toString() {
-
-        return super.toString() +" Score=" + Score;
+        return super.toString() + " Score=" + Score;
     }
+
+/*    @Override
+    public String toString() {
+        return super.toString()+
+                "Score=" + Score +
+                ", services=" + services;
+    }*/
+
+    /*@PreRemove
+    public void removeExpertFromSubservice() {
+        for (SubService s : services) {
+            s.getExperts().remove(this);
+        }
+    }*/
+
+  /*  public  void saveImage() {
+        File file = new File("C:\\image\\" + this.getName() + "");
+        byte[] imageData = new byte[(int) file.length()];
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            fileInputStream.read(imageData);
+            fileInputStream.close();
+        } catch (
+                Exception e) {
+            e.printStackTrace();
+        }
+        this.setImage(imageData);
+    }*/
 }
---
+++ src/main/java/ir/maktab/homeServiceProvider/util/ImageWrapper.java
@@ -0,0 +1,30 @@
+package ir.maktab.homeServiceProvider.util;
+
+import ir.maktab.homeServiceProvider.model.entity.Expert;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+
+public class ImageWrapper {
+    private int maxFileSize =350 * 350;
+
+    public static void saveImage(String picName, Expert expert) {
+        File file = new File("C:\\image\\" + picName + "");
+        int length = (int) file.length();
+        if (length < 15000 * 15000) {
+            byte[] imageData = new byte[length];
+            try {
+                FileInputStream fileInputStream = new FileInputStream(file);
+                fileInputStream.read(imageData);
+                fileInputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            expert.setImage(imageData);
+        } else
+            throw new RuntimeException("image is too large to upload");
+    }
+
+
+}
