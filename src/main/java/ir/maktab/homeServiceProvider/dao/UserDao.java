6a38d7579872c1c5279a59dba82993400670d1dc
neda <nedaakbari555@gmail.com>
2022-01-05 00:50:29 +0330

    findUsersByFilter method added to userDao

--- src/main/java/ir/maktab/homeServiceProvider/dao/UserDao.java
+++ src/main/java/ir/maktab/homeServiceProvider/dao/UserDao.java
@@ -1,12 +1,20 @@
 package ir.maktab.homeServiceProvider.dao;
 
+import ir.maktab.homeServiceProvider.model.dto.UserDto;
 import ir.maktab.homeServiceProvider.model.entity.Customer;
 import ir.maktab.homeServiceProvider.model.entity.User;
 import ir.maktab.homeServiceProvider.util.HibernateUtil;
+import ir.maktab.homeServiceProvider.util.filter.UserFilter;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
 import org.hibernate.query.Query;
+import org.hibernate.transform.Transformers;
 
 import java.util.List;
 /**
@@ -77,6 +85,49 @@ public class UserDao {
         transaction.commit();
         session.close();
         return user;
+    }
+    public List<UserDto> findUsersByFilter(UserFilter filter) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria criteria = session.createCriteria(User.class, "u");
+        if (filter.getRole() != null) {
+            SimpleExpression filterByRole = Restrictions.eq("u.role", filter.getRole());
+            criteria.add(filterByRole);
+        }
+        if (filter.getStatus() != null) {
+            SimpleExpression filterByStatus = Restrictions.eq("u.status", filter.getStatus());
+            criteria.add(filterByStatus);
+        }
+        if (filter.getName() != null) {
+            SimpleExpression filterByName = Restrictions.eq("u.name", filter.getName());
+            criteria.add(filterByName);
+        }
+        if (filter.getLastName() != null) {
+            SimpleExpression filterByFamily = Restrictions.eq("u.lastName", filter.getLastName());
+            criteria.add(filterByFamily);
+        }
+        if (filter.getStartDate() != null && filter.getEndDate() != null) {
+            Criterion between = Restrictions.between("u.registerDate", filter.getStartDate(), filter.getEndDate());
+            criteria.add(between);
+        }
+
+
+        criteria.setProjection(Projections.projectionList()
+                .add(Projections.property("u.registerDate").as("registerDate"))
+                .add(Projections.property("u.role").as("role"))
+                .add(Projections.property("u.name").as("name"))
+                .add(Projections.property("u.lastName").as("lastName"))
+                .add(Projections.property("u.role").as("role"))
+                .add(Projections.property("u.phoneNumber").as("phoneNumber"))
+                .add(Projections.property("u.email").as("email"))
+                .add(Projections.property("u.status").as("status"))
+                .add(Projections.property("u.id").as("id")));
+
+        criteria.setResultTransformer(Transformers.aliasToBean(UserDto.class));
+        List<UserDto> list = criteria.list();
+        transaction.commit();
+        session.close();
+        return list;
     }
 
 
