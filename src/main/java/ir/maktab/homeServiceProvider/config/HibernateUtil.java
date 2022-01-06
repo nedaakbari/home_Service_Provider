941a2d287868febdff43f94395334f68460db094
neda <nedaakbari555@gmail.com>
2022-01-06 17:15:28 +0330

    change spring config from class to annotation

--- src/main/java/ir/maktab/homeServiceProvider/config/HibernateUtil.java
+++ src/main/java/ir/maktab/homeServiceProvider/config/HibernateUtil.java
@@ -1,7 +1,65 @@
 package ir.maktab.homeServiceProvider.config;
 
+
+import ir.maktab.homeServiceProvider.model.entity.*;
+import ir.maktab.homeServiceProvider.model.entity.Person.Admin;
+import ir.maktab.homeServiceProvider.model.entity.Person.Customer;
+import ir.maktab.homeServiceProvider.model.entity.Person.Expert;
+import ir.maktab.homeServiceProvider.model.entity.Person.User;
+import ir.maktab.homeServiceProvider.model.entity.service.MainService;
+import ir.maktab.homeServiceProvider.model.entity.service.SubService;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 /**
  * author: neda akbari
  */
 public class HibernateUtil {
+
+    public static SessionFactory sessionFactory;
+
+    public static SessionFactory buildSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                {
+                    Configuration configuration = new Configuration();
+                    Properties setting = new Properties();
+/*
+                    <property name="hibernate.current_session_context_class">thread</property>
+*/
+                    setting.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                    setting.put(Environment.URL, "jdbc:mysql://localhost:3306/home_service");
+                    setting.put(Environment.USER, "neda");
+                    setting.put(Environment.PASS, "13730203@Neda");
+                    setting.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+                    setting.put(Environment.SHOW_SQL, "false");
+                    setting.put(Environment.FORMAT_SQL, "false");
+                    setting.put(Environment.HBM2DDL_AUTO, "update");
+
+                    configuration.setProperties(setting);
+                    configuration.addAnnotatedClass(User.class);
+                    configuration.addAnnotatedClass(Admin.class);
+                    configuration.addAnnotatedClass(Expert.class);
+                    configuration.addAnnotatedClass(Customer.class);
+                    configuration.addAnnotatedClass(SubService.class);
+                    configuration.addAnnotatedClass(MainService.class);
+                    configuration.addAnnotatedClass(Transaction.class);
+                    configuration.addAnnotatedClass(Offer.class);
+                    configuration.addAnnotatedClass(Orders.class);
+
+                    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                            .applySettings(configuration.getProperties()).build();
+                    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
 }
--- src/main/java/ir/maktab/homeServiceProvider/config/SpringConfig.java
+++ src/main/java/ir/maktab/homeServiceProvider/config/SpringConfig.java
@@ -10,56 +10,15 @@ import ir.maktab.homeServiceProvider.service.MainServiceService;
 import ir.maktab.homeServiceProvider.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@ComponentScan(basePackages = {"ir.maktab.homeServiceProvider.dao","ir.maktab.homeServiceProvider.service"})
+//@ComponentScan("ir.maktab")
+@Configuration
 /**
  * author: neda akbari
  */
-@Configuration
 public class SpringConfig {
-    @Bean
-    public UserDao userDao() {
-        return new UserDao();
-    }
 
-    @Bean
-    public UserService userService(UserDao userDao) {
-        UserService userService = new UserService();
-        userService.setUserDao(userDao);
-        return userService;
-    }
-
-    @Bean
-    public ExpertDao expertDao() {
-        return new ExpertDao();
-    }
-
-    @Bean
-    public ExpertService expertService(ExpertDao expertDao) {
-        ExpertService expertService = new ExpertService();
-        expertService.setExpertDao(expertDao);
-        return expertService;
-    }
-
-    @Bean
-    public AdminDao adminDao() {
-        return new AdminDao();
-    }
-
-    @Bean
-    public AdminService adminService(AdminDao adminDao) {
-        AdminService adminService = new AdminService();
-        adminService.setAdminDao(adminDao);
-        return adminService;
-    }
-
-    @Bean
-    public MainServiceDao mainServiceDao() {
-        return new MainServiceDao();
-    }
-
-    @Bean
-    public MainServiceService mainServiceService(MainServiceDao mainServiceDao) {
-        MainServiceService mainServiceService = new MainServiceService();
-        mainServiceService.setMainServiceDao(mainServiceDao);
-        return mainServiceService;
-    }
 }
