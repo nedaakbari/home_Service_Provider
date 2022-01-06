16ebe7130c8fe04b7901c20e05976d3c4710ab90
neda <nedaakbari555@gmail.com>
2022-01-06 17:38:13 +0330

    delete HomeService Enum because of useless

--- src/main/java/ir/maktab/homeServiceProvider/model/enumration/HomeAppliances.java
+++
@@ -1,8 +0,0 @@
-package ir.maktab.homeServiceProvider.model.enumration;
-
-public enum HomeAppliances {
-    KITCHEN_APPLIANCES,//لوازم اشپزخانه
-    LAUNDRY,//لباس شویی
-    AUDIO_VIDEO_EQUIPMENT//لوازم صوتی تصویری
-
-}
--- src/main/java/ir/maktab/homeServiceProvider/model/enumration/HomeCleaning.java
+++
@@ -1,9 +0,0 @@
-package ir.maktab.homeServiceProvider.model.enumration;
-
-public enum HomeCleaning {
-    CLEANING,//نظافت
-    LAUNDRY_AND_CAR_WASH,//لباسشویی و کارواش
-    CARPET_AND_UPHOLSTERY,//فرش و اثاثیه
-    HOME_SPRAYING//سمپاشی منازل
-
-}
