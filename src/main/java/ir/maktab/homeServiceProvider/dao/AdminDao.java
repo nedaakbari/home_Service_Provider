93cfdc8c9b22809a5bd2274107b6d9689e3fbfc9
neda <nedaakbari555@gmail.com>
2022-01-06 17:17:27 +0330

    change out method of adminDao to optional

--- src/main/java/ir/maktab/homeServiceProvider/dao/AdminDao.java
+++ src/main/java/ir/maktab/homeServiceProvider/dao/AdminDao.java
@@ -2,17 +2,26 @@ package ir.maktab.homeServiceProvider.dao;
 
 import ir.maktab.homeServiceProvider.model.entity.Admin;
 import ir.maktab.homeServiceProvider.model.entity.User;
-import ir.maktab.homeServiceProvider.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import java.util.List;
+import ir.maktab.homeServiceProvider.config.HibernateUtil;
+import ir.maktab.homeServiceProvider.model.entity.Person.Admin;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * author: neda akbari
  */
+@Component
 public class AdminDao {
     private SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
 
@@ -40,22 +49,23 @@ public class AdminDao {
         session.close();
     }
 
-    public List<User> findAll() {
+    public List<Admin> findAll() {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         Query query = session.createQuery("from Admin ");
-        List<User> users = query.list();
+        List<Admin> admins = query.list();
         transaction.commit();
         session.close();
-        return users;
+        return admins;
     }
-    public Admin findByUseAndPass(String userName, String password) {
+
+    public Optional<Admin> findByUseAndPass(String userName, String password) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("From Admin A Where A.passWord = :password and  A.userName=:username");
+        Query<Admin> query = session.createQuery("From Admin A Where A.passWord = :password and  A.userName=:username");
         query.setParameter("username", userName);
         query.setParameter("password", password);
-        Admin admin = (Admin) query.uniqueResult();
+        Optional<Admin> admin = Optional.ofNullable(query.uniqueResult());
         transaction.commit();
         session.close();
         return admin;
