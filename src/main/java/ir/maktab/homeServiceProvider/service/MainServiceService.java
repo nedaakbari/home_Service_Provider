b6447a0e5e282002994ae9f3051b5bc338d9d6af
neda <nedaakbari555@gmail.com>
2022-01-06 17:36:38 +0330

    OfferService class added

--- src/main/java/ir/maktab/homeServiceProvider/service/MainServiceService.java
+++ src/main/java/ir/maktab/homeServiceProvider/service/MainServiceService.java
@@ -2,14 +2,71 @@ package ir.maktab.homeServiceProvider.service;
 
 import ir.maktab.homeServiceProvider.dao.MainServiceDao;
 import ir.maktab.homeServiceProvider.model.entity.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
+@Component
 public class MainServiceService {
     MainServiceDao mainServiceDao;
 
-    public void save(MainService mainService) {
-        mainServiceDao.save(mainService);
+    public void saveMainService(MainService mainService) {
+        Optional<MainService> foundMainService = mainServiceDao.findByName(mainService.getName());
+        if (foundMainService.isPresent()) {
+            throw new RuntimeException("❌❌❌ this main service is already exist ❌❌❌");
+        } else {
+            mainServiceDao.save(mainService);
+        }
     }
 
+    public void deleteMainService(MainService mainService) {
+        Optional<MainService> foundMainService = mainServiceDao.findByName(mainService.getName());
+        if (foundMainService.isPresent()) {
+            mainServiceDao.delete(mainService);
+        } else {
+            throw new RuntimeException("❌❌❌ there is no  main service with this info ❌❌❌");
+        }
+    }
+
+    public void updateMainService(MainService mainService) {
+        mainServiceDao.update(mainService);
+    }
+
+    public List<MainService> findAllMainService() {
+        List<MainService> all = mainServiceDao.findAll();
+        if (all.size() != 0) {
+            return all;
+        } else
+            throw new RuntimeException("no mainService Exist");
+    }
+
+    public MainService findByName(String name) {
+        Optional<MainService> main = mainServiceDao.findByName(name);
+        if (main.isPresent()) {
+            return main.get();
+        } else
+            throw new RuntimeException("there is no mainService With this name");
+    }
+
+    public MainService findById(int mainId) {
+        Optional<MainService> main = mainServiceDao.findById(mainId);
+        if (main.isPresent()) {
+            return main.get();
+        } else
+            throw new RuntimeException("there is no mainService With this id");
+    }
+
+    public List<MainService> findAll() {
+        return mainServiceDao.findAll();
+    }
+
+    //region getter & setter & constructor
+    @Autowired
+    public MainServiceService(MainServiceDao mainServiceDao) {
+        this.mainServiceDao = mainServiceDao;
+    }
 
     public MainServiceDao getMainServiceDao() {
         return mainServiceDao;
@@ -18,4 +75,5 @@ public class MainServiceService {
     public void setMainServiceDao(MainServiceDao mainServiceDao) {
         this.mainServiceDao = mainServiceDao;
     }
+    //endregion
 }
---
+++ src/main/java/ir/maktab/homeServiceProvider/service/OfferService.java
@@ -0,0 +1,59 @@
+package ir.maktab.homeServiceProvider.service;
+
+import ir.maktab.homeServiceProvider.dao.OfferDao;
+import ir.maktab.homeServiceProvider.model.entity.Offer;
+import ir.maktab.homeServiceProvider.model.entity.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class OfferService {
+    OfferDao offerDao;
+
+    public void saveOffer(Offer offer) {
+        offerDao.save(offer);
+    }
+
+    public void deleteOffer(Offer offer) {
+        offerDao.delete(offer);
+    }
+
+    public void updateOffer(Offer offer) {
+        offerDao.update(offer);
+    }
+
+    public List<Offer> findAllOffer() {
+        List<Offer> all = offerDao.findAll();
+        if (all.size() != 0) {
+            return all;
+        } else
+            throw new RuntimeException("no offer Exist yet");
+    }
+
+    public List<Offer> findAllOfferOfAnOrder(int OrderId) {
+        List<Offer> all = offerDao.findAllOfferOfAnOrder(OrderId);
+        if (all.size() != 0) {
+            return all;
+        } else
+            throw new RuntimeException("no offer for this order Exist yet ");
+    }
+
+    public Offer findOfferById(int id) {
+        return offerDao.findOfferById(id);
+    }
+
+    //region setter & getter & constructor
+    @Autowired
+    public OfferService(OfferDao offerDao) {
+        this.offerDao = offerDao;
+    }
+
+    public OfferDao getOfferDao() {
+        return offerDao;
+    }
+    public void setOfferDao(OfferDao offerDao) {
+        this.offerDao = offerDao;
+    }
+    //endregion
+}
