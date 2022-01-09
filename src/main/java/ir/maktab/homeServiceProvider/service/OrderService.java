f37f2ea3374aa3460dfcbc96d3ef0af5751b25a2
neda <nedaakbari555@gmail.com>
2022-01-09 18:43:48 +0330

    modify OrderService

--- src/main/java/ir/maktab/homeServiceProvider/service/OrderService.java
+++ src/main/java/ir/maktab/homeServiceProvider/service/OrderService.java
@@ -7,18 +7,19 @@ import ir.maktab.homeServiceProvider.data.model.enumeration.OrderState;
 import ir.maktab.homeServiceProvider.dto.mapper.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 public class OrderService {
     Mapper mapper = new Mapper();
     OrderDao orderDao;
 
 
-    public void saveOrder(Orders orders) throws Exception {
+/*    public void saveOrder(Orders orders) throws Exception {
         orders.setState(OrderState.WAITING_FOR_EXPERT_SUGGESTION);
         long baseAmount = orders.getSubService().getBaseAmount();
         long proposedPrice = orders.getProposedPrice();
@@ -26,16 +27,16 @@ public class OrderService {
             orderDao.save(orders);
         } else
             throw new Exception("your proposedPrice must be more than base amount of this service");
-    }
+    }*/
 
-    public void update(Orders orders) {
+  /*  public void update(Orders orders) {
         orderDao.update(orders);
     }
 
     public List<OrdersDto> findOrdersOfSubService(int subServiceId) {
         List<Orders> orders = orderDao.findOrdersOfSubService(subServiceId);
         return orders.stream().map(mapper::ordersDto).collect(Collectors.toList());
-    }
+    }*/
 
     public Orders findOrderByID(int id) {
         Optional<Orders> foundOrder = orderDao.findOrderByID(id);
