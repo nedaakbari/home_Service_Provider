4326b3835ed9aad2677ec0d90b0f170caba2c53a
neda <nedaakbari555@gmail.com>
2022-01-09 17:59:36 +0330

    make MainServiceDao as interface

--- src/main/java/ir/maktab/homeServiceProvider/data/dao/MainServiceDao.java
+++ src/main/java/ir/maktab/homeServiceProvider/data/dao/MainServiceDao.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -12,64 +13,24 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class MainServiceDao {
+public interface MainServiceDao extends PagingAndSortingRepository<MainService, Integer> {
 
-    public void save(MainService mainService) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(mainService);
-        transaction.commit();
-        session.close();
-    }
-
-    public void update(MainService mainService) {
+    //void save
+    // void delete(MainService mainService)
+//List<MainService> findAll()
+   /* public void update(MainService mainService) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         session.update(mainService);
         transaction.commit();
         session.close();
-    }
+    }*/
 
-    public void delete(MainService mainService) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(mainService);
-        transaction.commit();
-        session.close();
-    }
+    //"From MainService M Where M.name=:name"
+    Optional<MainService> findByName(String name);
+    //"From MainService M Where M.id=:id"
+    Optional<MainService> findById(int mainId);
 
-    public List<MainService> findAll() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from MainService ");
-        List<MainService> mainService = query.list();
-        //users = session.createQuery("from User ").list();
-        transaction.commit();
-        session.close();
-        return mainService;
-    }
-
-    public Optional<MainService> findByName(String name) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query<MainService> query = session.createQuery("From MainService M Where M.name=:name");
-        query.setParameter("name", name);
-        Optional<MainService> mainService = Optional.ofNullable(query.uniqueResult());
-        transaction.commit();
-        session.close();
-        return mainService;
-    }
-
-    public Optional<MainService> findById(int mainId) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query<MainService> query = session.createQuery("From MainService M Where M.id=:id");
-        query.setParameter("id", mainId);
-        Optional<MainService> mainService = Optional.ofNullable(query.uniqueResult());
-        transaction.commit();
-        session.close();
-        return mainService;
-    }
 
 
 }
