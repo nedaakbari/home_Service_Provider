5ab8d224686012d081322ae906dd9c0984c35a4b
neda <nedaakbari555@gmail.com>
2022-01-09 18:13:36 +0330

    make TransActionDao as interface

--- src/main/java/ir/maktab/homeServiceProvider/data/dao/TransActionDao.java
+++ src/main/java/ir/maktab/homeServiceProvider/data/dao/TransActionDao.java
@@ -4,30 +4,16 @@ import ir.maktab.homeServiceProvider.data.model.entity.TransAction;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
-import org.springframework.stereotype.Component;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public class TransActionDao {
+public interface TransActionDao extends PagingAndSortingRepository<TransAction,Long> {
 
-    public void save(TransAction transAction) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(transAction);
-        transaction.commit();
-        session.close();
-    }
+    //public void save(TransAction transAction) ;
 
-    public List<TransAction> findAll() {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Query query = session.createQuery("from TransAction ");
-        List<TransAction> transActions = query.list();
-        transaction.commit();
-        session.close();
-        return transActions;
-    }
+    //public List<TransAction> findAll() ;
 }
