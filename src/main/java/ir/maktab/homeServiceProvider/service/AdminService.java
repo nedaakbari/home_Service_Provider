bc0babdded81441e26bda8300e9003928e5b18a9
neda <nedaakbari555@gmail.com>
2022-01-18 00:02:25 +0330

    AdminService modify with implements Services

--- src/main/java/ir/maktab/homeServiceProvider/service/AdminService.java
+++ src/main/java/ir/maktab/homeServiceProvider/service/AdminService.java
@@ -1,7 +1,10 @@
 package ir.maktab.homeServiceProvider.service;
 
 import ir.maktab.homeServiceProvider.data.dao.AdminDao;
+import ir.maktab.homeServiceProvider.data.model.entity.Address;
 import ir.maktab.homeServiceProvider.data.model.entity.Person.Admin;
+import ir.maktab.homeServiceProvider.dto.AddressDto;
+import ir.maktab.homeServiceProvider.dto.AdminDto;
 import ir.maktab.homeServiceProvider.dto.mapper.AdminMapper;
 import ir.maktab.homeServiceProvider.exception.NotFoundDta;
 import lombok.RequiredArgsConstructor;
@@ -12,31 +15,37 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class AdminService {
+public class AdminService implements Services<Admin, AdminDto, Integer> {
     private final AdminMapper mapper;
     private final AdminDao adminDao;
 
-    public void saveAdmin(Admin admin) {
+    @Override
+    public void save(Admin admin) {
         adminDao.save(admin);
     }
 
-    public void removeAdmin(Admin admin) {
+    @Override
+    public void delete(Admin admin) {
         adminDao.delete(admin);
+
     }
 
-    public List<Admin> findAllAdmins() {
-        List<Admin> list = new ArrayList<>();
-        Iterable<Admin> admins = adminDao.findAll();
-        admins.forEach(list::add);
-        return list;
+    @Override
+    public List<AdminDto> getAll() {
+        List<Admin> allAdmin = adminDao.findAll();
+        return allAdmin.stream().map(mapper::adminToDto).collect(Collectors.toList());
     }
 
-    @Transactional
-    public void UpdatePassword(String newPassword, int id) {
-        adminDao.updatePassword(newPassword, id);
+    @Override
+    public Admin getById(Integer theId) {
+        Optional<Admin> found = adminDao.findById(theId);
+        if (found.isPresent())
+            return found.get();
+        else throw new NotFoundDta("no admin found ");
     }
 
     public Admin findAminByUseAndPass(String username, String password) {
@@ -46,10 +55,15 @@ public class AdminService {
         } else
             throw new NotFoundDta("no admin found with these use and pass");
     }
-    /*  public Admin findAminByUseAndPass(Admin admin) {
+        /*  public Admin findAminByUseAndPass(Admin admin) {
         Optional<Admin> found = adminDao.findByUserNameAndPassWord(admin.getUserName(), admin.getPassWord());
         return found.orElse(null);
     }*///todo which one is better
+
+    @Transactional
+    public void UpdatePassword(String newPassword, int id) {
+        adminDao.updatePassword(newPassword, id);
+    }
 
 
     public boolean isExist(String username, String password) {
@@ -59,14 +73,5 @@ public class AdminService {
         else return false;
     }
 
-    public Iterable<Admin> findAll() {
-        return adminDao.findAll();
-    }
-
-
-   /* @Autowired
-    public AdminService(AdminDao adminDao) {
-        this.adminDao = adminDao;
-    }*/
 
 }
