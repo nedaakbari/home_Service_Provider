c836720c8fa924b6c2fb0d3f729abdadf974b202
neda <nedaakbari555@gmail.com>
2022-01-18 01:14:18 +0330

    CommentService modify with implements Services

--- src/main/java/ir/maktab/homeServiceProvider/dto/mapper/CommentMapper.java
+++ src/main/java/ir/maktab/homeServiceProvider/dto/mapper/CommentMapper.java
@@ -1,8 +1,13 @@
 package ir.maktab.homeServiceProvider.dto.mapper;
 
 
+import ir.maktab.homeServiceProvider.data.model.entity.Comment;
+import ir.maktab.homeServiceProvider.dto.CommentDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentMapper {
+    public CommentDto commentToDto(Comment comment) {
+        return null;
+    }
 }
--- src/main/java/ir/maktab/homeServiceProvider/service/CommentService.java
+++ src/main/java/ir/maktab/homeServiceProvider/service/CommentService.java
@@ -1,14 +1,50 @@
 package ir.maktab.homeServiceProvider.service;
 
 import ir.maktab.homeServiceProvider.data.dao.CommentDao;
+import ir.maktab.homeServiceProvider.data.model.entity.Comment;
+import ir.maktab.homeServiceProvider.dto.CommentDto;
 import ir.maktab.homeServiceProvider.dto.mapper.CommentMapper;
+import ir.maktab.homeServiceProvider.exception.NotFoundDta;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
-public class CommentService {
+public class CommentService implements Services<Comment, CommentDto, Long> {
     private final CommentMapper mapper;
     private final CommentDao commentDao;
 
+    @Override
+    public void save(Comment comment) {
+        commentDao.save(comment);
+    }
+
+    @Override
+    public void delete(Comment comment) {
+        commentDao.delete(comment);
+    }
+
+    @Override
+    public List<CommentDto> getAll() {
+        List<Comment> allComments = commentDao.findAll();
+        return allComments.stream().map(mapper::commentToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public Comment getById(Long theId) {
+        Optional<Comment> found = commentDao.findById(theId);
+        if (found.isPresent())
+            return found.get();
+        else throw new NotFoundDta("no comment");
+    }
+
+
+    public List<CommentDto> findAllCommentOfAnOrder(){
+        //todo
+        return null;
+    }
 }
